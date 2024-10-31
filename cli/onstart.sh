603f07c359caffd9eefd2e17a0a333848aece313
Andrii Prudnikov <a.prudnikov@gmail.com>
2024-10-31 06:45:53 -0300

    Better provisioning

--- cli/onstart.sh
+++ cli/onstart.sh
@@ -2,16 +2,64 @@
 
 env >> /etc/environment;
 mkdir -p ${DATA_DIRECTORY:-/workspace}
+
+
+# ----------------------------------------------------------
+# Bootstrap python project
+# ----------------------------------------------------------
+# Install python prequities
 apt-get -y install libgl1
 apt-get -y install python3-pip
 pip3 install --upgrade pip setuptools
+
+# Clone python project and install dependencies
 mkdir ~/projects
+cd ~/projects/
 ssh-keyscan github.com >> ~/.ssh/known_hosts
-cd ~/projects
 git clone https://github.com/aprudnikoff/Real-ESRGAN-impl.git
 cd Real-ESRGAN-impl/
 pip3 install -e .
 # python3 setup.py develop
+
+
+# ----------------------------------------------------------
+# Bootstrap JS project
+# ----------------------------------------------------------
+
+# Add ssh key for bitbucket
+echo "-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACDjlPbeg+G+0T80vbjPb7ae/E2YYcvw0ioeu9c3zAOFbgAAAJiw9W81sPVv
+NQAAAAtzc2gtZWQyNTUxOQAAACDjlPbeg+G+0T80vbjPb7ae/E2YYcvw0ioeu9c3zAOFbg
+AAAEDbjPimrtyANiUqYK2KABjkxpkgTT5k/DYPZ7iScTIwZuOU9t6D4b7RPzS9uM9vtp78
+TZhhy/DSKh671zfMA4VuAAAAFWEucHJ1ZG5pa292QGdtYWlsLmNvbQ==
+-----END OPENSSH PRIVATE KEY-----
+" >> ~/.ssh/id_ed25519
+chmod 400 ~/.ssh/id_ed25519
+
+
+# Install nvm and node
+mkdir ~/temp
+cd ~/temp/
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 20
+
+
+# Clone JS project and install dependencies
+cd ~/projects/
+ssh-keyscan bitbucket.org >> ~/.ssh/known_hosts
+git clone git@bitbucket.org:pxx/stock-ass.git
+cd stock-ass/
+git checkout master
+npm i
+
+
+
+
+
 
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "Caralho!!! All done!"
--- cli/search.sh
+++ cli/search.sh
@@ -14,20 +14,29 @@ echo Finding offers
 OFFERS="$(vastai search offers 'num_gpus=1 dph<0.06 storage_cost<2 rented=False' --on-demand --storage 16)"
 OFFER_ID=$(awk 'NR==2{print $1}' <<< "${OFFERS}")
 echo "$OFFERS"
+
+echo
 echo "Picking offer id=${OFFER_ID}"
 
 
+echo
 echo Creating instance
 # Examples:
 # vastai create instance 6995713 --image pytorch/pytorch --disk 40 --env '-p 8081:80801/udp -h billybob' --ssh --direct --onstart-cmd "env | grep _ >> /etc/environment; echo 'starting up'";
 # vastai create instance 384827  --image bobsrepo/pytorch:latest --login '-u bob -p 9d8df!fd89ufZ docker.io' --jupyter --direct --env '-e TZ=PDT -e XNAME=XX4 -p 22:22 -p 8080:8080' --disk 20
-CREATE_RESPONSE="$(vastai create instance ${OFFER_ID} --raw --image nvidia/cuda:12.5.1-cudnn-devel-ubuntu22.04 --disk 16 --ssh --jupyter --env '-e DATA_DIRECTORY=/workspace/ -e JUPYTER_DIR=/' --onstart onstart.sh)"
+IMAGE_TAG='nvidia/cuda:12.5.1-cudnn-devel-ubuntu22.04'
+DISK_SIZE=16
+OPTIONS='--ssh --jupyter --jupyter-lab'
+ENV_VARS="-e DATA_DIRECTORY=/workspace/ -e JUPYTER_DIR=/"
+CREATE_RESPONSE="$(vastai create instance ${OFFER_ID} --raw --image ${IMAGE_TAG} --disk ${DISK_SIZE} ${OPTIONS} --env "${ENV_VARS}" --onstart onstart.sh)"
 echo "$CREATE_RESPONSE"
 INSTANCE_ID="$(jq -r '.new_contract' <<< "$CREATE_RESPONSE")"
 echo "Created instance id=${INSTANCE_ID}"
 
 
+echo
 echo Waiting for an instance to start...
+start=`date +%s`
 while true; do
   # Execute the command and capture its output
   STATUS_RESPONSE=$(vastai show instance ${INSTANCE_ID} --raw)
@@ -44,15 +53,14 @@ while true; do
   printf '.'
   sleep 5
 done
-# TODO: done in N seconds
-
-echo Adding ssh key to access stock-ass repo
-ssh-keyscan -p ${SSH_PORT} ${SSH_HOST} >> ~/.ssh/known_hosts
-scp -P ${SSH_PORT} id_rsa id_rsa.pub root@${SSH_HOST}:~/.ssh/
+end=`date +%s`
+runtime=$((end-start))
+echo "Done in ${runtime} seconds"
 
 
-
-echo Waiting for a instance to complete provisioning...
+echo
+echo Waiting for an instance to complete provisioning...
+start=`date +%s`
 while true; do
   # Execute the command and capture its output
   INSTANCE_LOGS=$(vastai logs ${INSTANCE_ID})
@@ -66,11 +74,9 @@ while true; do
   printf '.'
   sleep 5
 done
-# TODO: done in N seconds
+end=`date +%s`
+runtime=$((end-start))
+echo "Done in ${runtime} seconds"
 
-
+echo
 echo "ssh -p ${SSH_PORT} root@${SSH_HOST} -L 8080:localhost:8080"
-
-
-ssh -p ${SSH_PORT} root@${SSH_HOST} 'bash -s' < onstart2.sh
-ssh -p 35094 root@ssh8.vast.ai 'bash -s' < onstart2.sh
